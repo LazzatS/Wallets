b886ae5c85514cacb6f700a5effbd226f9c4e4ef
LazzatS <lseiilova@nu.edu.kz>
2022-01-20 23:26:23 +0600

    Initial commit

---
+++ Wallets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wallets
+//
+//  Created by Lazzat Seiilova on 20.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
